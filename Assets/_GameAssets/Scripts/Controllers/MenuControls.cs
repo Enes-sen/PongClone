acfee6104fe276e54da5099ee7f9b7489ba96938
darklord <113107334+Enes-sen@users.noreply.github.com>
2026-09-23 16:23:10 +0300

    PongClone:State:Menu Sfx update

--- Assets/_GameAssets/Scripts/Controllers/MenuControls.cs
+++ Assets/_GameAssets/Scripts/Controllers/MenuControls.cs
@@ -1,10 +1,14 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MenuControls : MonoBehaviour
 {
+    [Header("buttons")]
+    [SerializeField] private Button playB,closeB;
+    [Header("Sfx Settings")]
+    [SerializeField] private AudioSource _Audiosource;
+    [SerializeField] private AudioClip SoundClip;
     private void Awake()
     {
         Time.timeScale = 1f;
@@ -13,11 +17,28 @@ public class MenuControls : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        playB.onClick.AddListener(PlayGame);
+        closeB.onClick.AddListener(CloseGame);
+
     }
-    public void PlayB()
+    private void PlayGame()
     {
-        Debug.Log("Loading Scene:" +1.ToString());
+        StartCoroutine(PlayGameC(0.1f));
+    }
+
+    private IEnumerator PlayGameC(float time)
+    {
+        _Audiosource.PlayOneShot(SoundClip);
+        yield return new WaitForSeconds(time);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
     }
-    public void CloseGame() { Debug.Log("Test"); Application.Quit(); }
+
+    private IEnumerator CloseGameC(float time)
+    {
+        _Audiosource.PlayOneShot(SoundClip);
+        yield return new WaitForSeconds(time);
+        Application.Quit();
+    }
+
+    private void CloseGame() { StartCoroutine(CloseGameC(0.1f)); }
 }
